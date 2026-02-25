35c141705912dd0b241245880bb20ae770ddc507
MrManoj124 <manojmanorooban@gmail.com>
2026-02-25 23:06:37 +0530

    create a simple if-elif-else condition for Local_Ai_Agent

--- Local_Ai.py
+++ Local_Ai.py
@@ -3,3 +3,19 @@ print("Type 'exit' to quit.\n")
 
 While True:
     user_input = input("You : ").lower()
+
+    if user_input == "Exit" :
+    print("Bot : Goodbye!")
+    break
+
+    elif "hello" in user_input:
+        print("Bot : Hi there!")
+
+    elif "how are you" in user_input:
+        print("Bot : I'm just code , but I'm functioning perfectly!")
+
+    elif"your name" in user_input:
+        print("Bot : I am your Local AI Assistant.")
+
+    else:
+        print("Bot : I don't understand that yet.")
